ea15d99dec3a1fe0df137c75d740b1a6f64a1316
Marcus Matthews <mlmatthews75@gmail.com>
2023-08-03 19:01:29 -0400

    added date to top of webpage

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-  ````// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+
+var currentDay = document.getElementById("currentDay")
+
+const date = new Date();
+
+let day = date.getDate();
+console.log(day);
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+let currentDate = `${month}-${day}-${year}`;
+console.log(currentDate);
+
+document.getElementById("currentDay").innerHTML = currentDate;
+
+
+  // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
