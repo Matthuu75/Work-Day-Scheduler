b6bb8b3a0254dc6c505f359b518a72290835fd8a
Marcus Matthews <mlmatthews75@gmail.com>
2023-08-06 17:17:59 -0400

    added the localStorage, time with h:mm and am/pm, added function for the current time to display past, present or future colors depending on time of day

--- script.js
+++ script.js
@@ -1,51 +1,44 @@
-// $(document).ready(function () {
+$(document).ready(function () {
+  console.log("ready!")
 
-//   const timeBlockEl = $(".time-block");
+  const timeBlock = $(".time-block");
+  timeBlock.on('click', 'button', function () {
+    const thisId = $(this).closest('.time-block').attr('id');
+    const thisDescription = $(this).siblings('.description');
+    localStorage.setItem(thisId, thisDescription.val());
+  })
 
-//   timeBlockEl.on('click', 'button', function () {
-//     const thisId = $(this).closest('.time-block').attr('id');
-//     const thisInput = $(this).siblings('.description');
-//     localStorage.setItem(thisId, thisInput.val());
-//   })
+  timeBlock.each(function () {
 
-  
-  function displayDate() {
-    const currentDayEl = $("#currentDay")
-    currentDayEl.text(dayjs().format('MMMM DD, YYYY'));
+    hourTime = $(this).attr('id');
+
+    currentHour = dayjs().format('h');
+
+    if (hourTime < currentHour) {
+      $(this).addClass('past');
+      $(this).removeClass('present, future');
+    } 
+    
+    else if (hourTime > currentHour) {
+      $(this).addClass('future');
+      $(this).removeClass('present, past');
+    } 
+    
+    else {
+      $(this).addClass('present');
+      $(this).removeClass('past, future');
+    }
+
+    savedDescription = localStorage.getItem(hourTime);
+    $(this).find('.description').val(savedDescription)
+
+  })
+
+  function displayDateTime() {
+    const currentDayEl = $("#currentDay");
+    currentDayEl.text(dayjs().format('MMMM DD, YYYY h:mma'));
   }
-  displayDate()
-// }
 
-// )
+  setInterval(displayDateTime, 1000);
 
-
-//make grey for past times
-//make red for present times
-//make green for future times
-//add a variable to make the colors show up according to system time
-
-
-
-  // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+})
